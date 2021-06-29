5344dd585d75388b088f5faa7650469ef360e9b0
Hardik Parpelli <hardik.parpelli@sap.com>
2021-06-29 22:29:34 +0530

    Verifying an Alien Dictionary

---
+++ AlienLanguage.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static boolean isAlienSorted(String[] words, String order) {
+        for(int i=0; i<words.length-1; i++){
+            String current = words[i];
+            String next = words[i+1];
+            int clen = current.length();
+            int nlen = next.length();
+            boolean lastCheck = false;
+            for(int j = 0; j<clen && j<nlen; j++){
+                lastCheck = current.charAt(j) == next.charAt(j);
+                if(order.indexOf(current.charAt(j)) > order.indexOf(next.charAt(j))){
+                    return false;
+                }
+                else if (order.indexOf(current.charAt(j)) < order.indexOf(next.charAt(j))){
+                    break;
+                }
+            }
+            if(lastCheck && nlen < clen) return false;
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String[] words = {"hello","leetcode"};
+        String order = "hlabcdefgijkmnopqrstuvwxyz";
+
+        System.out.println(isAlienSorted(words, order));
+    }
+}
